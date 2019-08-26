b534d1fb4bb5b8f50876a271c2305906fbe20bbc
Gabriel Lévesque-Duval <glevesqueduval@gmail.com>
2019-08-26 14:18:43 -0400

    Création du fichier AParametres.java
    FAIT:
    * Creation de la première classe java
    TODO:
    * Code pour afficher "Bonjour!" dans le Log

---
+++ app/src/main/java/ca/cours5b5/gabriellevesqueduval/activites/AParametres.java
@@ -0,0 +1,16 @@
+package ca.cours5b5.gabriellevesqueduval.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cours5b5.gabriellevesqueduval.R;
+
+public class AParametres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_parametres);
+    }
+}
