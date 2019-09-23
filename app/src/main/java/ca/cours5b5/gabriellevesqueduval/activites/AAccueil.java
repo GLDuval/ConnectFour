3fea92fed75851512429f7317007b97042533c65
Gabriel Lévesque-Duval <glevesqueduval@gmail.com>
2019-09-23 14:23:11 -0400

    Fin de l'atelier 06 FAIT: * Ajout de l'évenement pour ouvrir l'activité APartieLocale * Ajout de l'évenement pour ouvrir l'activité AParamatres * Ajout des méthodes de cycle de vie des activités (onResume, onPause, onDestroy, onSaveInstanceState) * Journalisation du cycle de vie des activités TODO: * Le prochain atelier

--- app/src/main/java/ca/cours5b5/gabriellevesqueduval/activites/AAccueil.java
+++ app/src/main/java/ca/cours5b5/gabriellevesqueduval/activites/AAccueil.java
@@ -1,6 +1,8 @@
 package ca.cours5b5.gabriellevesqueduval.activites;
 
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 
 import ca.cours5b5.gabriellevesqueduval.R;
@@ -27,5 +29,23 @@ public class AAccueil extends ActiviteAvecControles {
         buttonJouer = findViewById(R.id.buttonJouer);
         buttonParametres = findViewById(R.id.buttonParametres);
         GLog.valeurs(buttonConnexion, buttonEnLigne, buttonJouer, buttonParametres);
+
+        buttonParametres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ouvertureParametres = new Intent(AAccueil.this, AParametres.class);
+                AAccueil.this.startActivity(ouvertureParametres);
+            }
+        });
+
+        buttonJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ouverturePartie = new Intent(AAccueil.this, APartieLocale.class);
+                AAccueil.this.startActivity(ouverturePartie);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/ca/cours5b5/gabriellevesqueduval/activites/Activite.java
+++ app/src/main/java/ca/cours5b5/gabriellevesqueduval/activites/Activite.java
@@ -19,5 +19,36 @@ public abstract class Activite extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+
+        GLog.appel(this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        GLog.appel(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        GLog.appel(this);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        GLog.appel(this);
+        super.onDestroy();
+    }
+
+
+
+
     protected abstract int getLayoutId();
 }
