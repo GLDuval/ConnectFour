139e0c21f4f1fb3f323f4134fb6847859de9a89b
Gabriel Lévesque-Duval <glevesqueduval@gmail.com>
2019-10-08 16:57:38 -0400

    Fin de l'atelier 8 FAIT: * Ajout des méthodes dans EntrepotDeDonnees qui permettent la sauvegarde des données dans un état. * Ajout de la dépendance à la libraire gson. * Journalisation des données chargées et sauvegardées. * Redéfinition de la méthode onSaveInstanceState dans ActiviteAvecModeles pour sauvegarder les données de l'état TODO: * L'atelier 10

--- app/src/main/java/ca/cours5b5/gabriellevesqueduval/donnees/EntrepotDeDonnees.java
+++ app/src/main/java/ca/cours5b5/gabriellevesqueduval/donnees/EntrepotDeDonnees.java
@@ -1,5 +1,12 @@
 package ca.cours5b5.gabriellevesqueduval.donnees;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,32 +14,87 @@ import ca.cours5b5.gabriellevesqueduval.global.GLog;
 
 public class EntrepotDeDonnees {
 
-    public static Map <Class<? extends Donnees>, Donnees> donneesMap = new HashMap<>();
+    public static Map<Class<? extends Donnees>, Donnees> donneesMap = new HashMap<>();
 
-    public static <D extends Donnees> D obtenirDonnees(Class<D> classeDonnees){
+    private static Gson gson = new GsonBuilder().create();
+
+    public static <D extends Donnees> D obtenirDonnees(Class<D> classeDonnees, Bundle etat){
 
         GLog.appel(EntrepotDeDonnees.class);
 
-        if(!siDonneesSontDansEntrepot(classeDonnees)){
-            try {
-                entreposerDonnees(creerDonnees(classeDonnees));
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+        D donnees;
+
+        if(!siDonneesSontDansEtat(classeDonnees, etat) ){
+            if(!siDonneesSontDansEntrepot(classeDonnees)){
+                donnees = creerDonnees(classeDonnees);
+                entreposerDonnees(donnees);
+            }else{
+                donnees = donneesDansEntrepot(classeDonnees);
             }
 
+
+        }else {
+            donnees = donneesDansEtat(classeDonnees, etat);
         }
-        return donneesDansEntrepot(classeDonnees);
+        return donnees;
 
     }
 
-    private static <D extends  Donnees> D creerDonnees(Class<D> classeDonnees) throws InstantiationException, IllegalAccessException {
+    private static <D extends  Donnees> D creerDonnees(Class<D> classeDonnees) {
+        GLog.appel(EntrepotDeDonnees.class);
+        D donnees = null;
+        try {
+            donnees = classeDonnees.newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        return donnees;
+
+    }
+    private static <D extends  Donnees> D donneesDansEtat(Class<D> classeDonnees, Bundle etat){
         GLog.appel(EntrepotDeDonnees.class);
 
-        return classeDonnees.newInstance();
+        String chaineJson = etat.getString(clePourClasseDonnees(classeDonnees));
+        D donnees = gson.fromJson(chaineJson, classeDonnees);
+        GLog.valeurs("Données chargées: ", classeDonnees, chaineJson );
+
+
+        return donnees;
+    }
+
+    private static boolean siDonneesSontDansEtat(Class<? extends Donnees> classeDonnees, @Nullable Bundle etat){
+        GLog.appel(EntrepotDeDonnees.class);
+
+        boolean present = false;
+        if(etat != null){
+            if(etat.containsKey(clePourClasseDonnees(classeDonnees))){
+                present = true;
+            }
+        }
+
+
+
+        return present;
+    }
+
+    private static String clePourClasseDonnees(Class<? extends Donnees> classeDonnees){
+        GLog.appel(EntrepotDeDonnees.class);
+
+        return classeDonnees.getSimpleName();
+    }
+
+    public static <D extends  Donnees> void sauvegarderDonnees(D donnees, Bundle outState){
+        GLog.appel(EntrepotDeDonnees.class);
+
+        String chaineJson = gson.toJson(donnees);
+        String cle = clePourClasseDonnees(donnees.getClass());
+        outState.putString(cle, chaineJson);
+        GLog.valeurs("Données sauvegardées: ", cle, chaineJson);
 
     }
+
     private static <D extends  Donnees> D donneesDansEntrepot(Class<? extends Donnees> classeDonnees){
         GLog.appel(EntrepotDeDonnees.class);
 
